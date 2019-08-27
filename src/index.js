6cf738410fa457bd3bc3c273515fc3897140877b
Ruth Baker <ruthmoog@gmail.com>
2019-08-27 14:46:11 +0100

    Use state to make an interactive component

--- src/index.js
+++ src/index.js
@@ -36,10 +36,20 @@ class Board extends React.Component {
 }
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button
+      className="square"
+      onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
