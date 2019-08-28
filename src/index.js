0c88c02d85d98f1c14df5fe213dd10b480a1f2c2
Ruth Baker <ruthmoog@gmail.com>
2019-08-28 12:07:34 +0100

    Change Square class into function component

--- src/index.js
+++ src/index.js
@@ -51,14 +51,12 @@ class Board extends React.Component {
   }
 }
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return(
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Game extends React.Component {
